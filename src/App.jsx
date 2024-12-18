a73dd2c6ded22ad6b64aa61b725500669b1c3e49
Betojr04 <betomasia12@gmail.com>
2024-12-17 20:30:59 -0700

    intialized vite react project and added project instructions to the read me file so people know what this project is and how it is supposed to work

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React,{ useState } from 'react';
+import "./App.css"
+
+const App = () => {
+
+  return(
+    <div>
+      <h1>Ecommerce Product Listing App</h1>
+      
+
+    </div>
+  )
+}
+
+export default App
